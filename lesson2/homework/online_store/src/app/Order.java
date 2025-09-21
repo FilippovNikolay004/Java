9f67aef673e2d8a4ae7975c5bcb7fe44dba30bbd
Николай Филиппов <filippovnikolay195@gmail.com>
2025-09-21 23:55:21 +0300

    hw

---
+++ lesson2/homework/online_store/src/app/Order.java
@@ -0,0 +1,24 @@
+package app;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private long id;
+    private String customerName;
+    private BigDecimal totalAmount;
+    private LocalDateTime createdAt;
+    private List<String> items;
+}
